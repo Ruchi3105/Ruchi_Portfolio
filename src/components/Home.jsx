cdb13d616a17696c54f3ddb86d197ab4baf6b40d
Ruchi3105 <ruchi.cse31@gmail.com>
2025-05-27 13:45:57 +0530

    contact button updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -27,7 +27,7 @@ const Home = () => {
             </a>
 
             <a
-              href="#projects"
+              href="#contact"
               className="flex items-center border border-blue-500 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200  hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)] hover:bg-blue-500/10 cursor-pointer"
             >
               Contacts
